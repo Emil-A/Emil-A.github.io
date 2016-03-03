a20639c0495f5e9a0a1a36d06ad8bff2889b4a30
Emil Aite <aite.emil@gmail.com>
2016-03-03 01:05:20 -0500

    playing around with some animations :)

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-function draw() {
-var canvas = document.getElementById('canvas');
-	if (canvas.getContext){
-		var ctx = canvas.getContext('2d');
+$(document).ready(function(){
+	$("#face1").animate({left: '250px'});
+	$("#face1").animate({top: '250px'});
+	$("#face1").animate({left: '20px'});
+	$("#face1").animate({top: '20px'});
 
-		var rectShape = new Path2D();
-		var rectDime = {x:10,y:20,width:20,height:40};
-		rectShape.rect(rectDime.x, rectDime.y, rectDime.width, rectDime.height,);
+	$("#face2").animate({left: '80px'});
+	$("#face2").animate({top: '250px'});
 
-		var circShape = new Path2D();
-		var circDime = {x:10,y:20,start:20,end:40,clock:2*Math.PI,moveX:125,moveY:35};
-		circShape.moveTo(circDime.moveX, circDime.moveY);
-		circShape.arc(circDime.x, circDime.y, circDime.start, circDime.end, circDime.clock);
-
-		ctx.fill(rectShape);
-		ctx.fillStyle = '#8ED6FF';
-		ctx.fill(circShape);
-	}
-}
+	$("#face3").animate({left: '250px'});
+	$("#face3").animate({top: '29px'});
+});
